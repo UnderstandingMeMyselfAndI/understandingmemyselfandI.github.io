34bb7db594d1cd87d1c14ac6c439b0bbc87b1bc3
Brendan Oliver <brendan.oliver@flpdigital.com>
2025-12-31 04:19:12 +0000

    phrases and lingo

--- src/components/ui/search/Search.jsx
+++ src/components/ui/search/Search.jsx
@@ -1,16 +1,39 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import SearchField from './SearchField'
-
+import Dialog from '../dialog/Dialog'
+import lingo from '@/data/lingo.js'
 import './styles.scss'
 const Search = () => {
+	const [showDialog, setShowDialog] = useState(false)
+	const [content, setContent] = useState([])
+	function getContent(id) {
+		if (!id) return
 
-    
+		return lingo.find((item) => {
+			if (item.id === id) return item.lingoFieldGroup
+		})
+	}
 
-
+	const handleClick = (id) => {
+		const search = getContent(id)
+		if (search?.lingoFieldGroup?.description) {
+			setContent(search)
+			setShowDialog(true)
+		}
+		console.log('clicked', search.lingoFieldGroup.description)
+	}
 	return (
 		<section className='search-lingo activity' id='lingo'>
+			<Dialog
+				show={showDialog}
+				title={content?.title}
+				instruction={content?.lingoFieldGroup?.description}
+				confirmLabel='Close'
+				onConfirm={() => setShowDialog(false)}
+				showCancel={false}
+			/>
 			<h3>Lingo &amp; Phrases</h3>
-			<SearchField />
+			<SearchField handleClick={handleClick} />
 		</section>
 	)
 }
--- src/components/ui/search/SearchField.jsx
+++ src/components/ui/search/SearchField.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import lingo from '@/data/lingo.js'
 import { useInView, useOnInView } from 'react-intersection-observer'
+import PropTypes from 'prop-types'
 import './styles.scss'
 function searchLingo(query) {
     if (!query || query.trim() === '') {
@@ -19,53 +20,38 @@ function searchLingo(query) {
 			return 0
 		})
 }
-function getContent(id) {
-    if (!id) return 	
-    
-    return lingo.find((item) => {
-        
-            if (item.id === id) return item.lingoFieldGroup
-    })
-    
-}
-const SearchField = () => {
-    const el = document.getElementById('search')
-    const [searchTerm, setSearchTerm] = useState('')
-    const[inView, setInView] = useState(false)
 
-    const filteredLingo = searchLingo(searchTerm)
-    
-    const inViewRef = useOnInView(
-			(inView) => {
-				if (inView) {
-					// Do something with the element that came into view
-					// console.log('Element is in view', entry.target)
-					setInView(true)
-				} else {
-					// console.log('Element left view', entry.target)
-                    setInView(false)
-                   
-				}
-			},
-			{
-				/* Optional options */
-				threshold: 0,
-				rootMargin: '-23% 0% -78% 0%',
-			}, // Optional IntersectionObserver options
-    )
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value)
-        if(e.target.value !== '') {
-            el.scrollInToView({ behavior: 'smooth', block: 'start' })
-        }
-    }
-    const handleClick = (id) => {
-        const search = getContent(id)
+const SearchField = ({ handleClick }) => {
+	const el = document.getElementById('search')
+	const [searchTerm, setSearchTerm] = useState('')
+	const [inView, setInView] = useState(false)
 
-        console.log('clicked', search.lingoFieldGroup.description)
-        
-    }
-   
+	const filteredLingo = searchLingo(searchTerm)
+
+	const inViewRef = useOnInView(
+		(inView) => {
+			if (inView) {
+				// Do something with the element that came into view
+				// console.log('Element is in view', entry.target)
+				setInView(true)
+			} else {
+				// console.log('Element left view', entry.target)
+				setInView(false)
+			}
+		},
+		{
+			/* Optional options */
+			threshold: 0,
+			rootMargin: '-23% 0% -78% 0%',
+		}, // Optional IntersectionObserver options
+	)
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value)
+		if (e.target.value !== '') {
+			el.scrollInToView({ behavior: 'smooth', block: 'start' })
+		}
+	}
+
 	return (
 		<div className='lingo-field ' ref={inViewRef} id='search'>
 			{/* <div>
@@ -80,12 +66,16 @@ const SearchField = () => {
 			<input className='' type='text' placeholder='Search Lingo & Phrases...' value={searchTerm} onChange={handleChange} />
 			<div className='lingo-list'>
 				{filteredLingo.map((item) => (
-					<div key={item.id} onClick={() => handleClick(item.id)}>{item.title}</div>
+					<div key={item.id} onClick={() => handleClick(item.id)}>
+						{item.title}
+					</div>
 				))}
 				{filteredLingo.length === 0 && <div className='no-results'>No results found</div>}
 			</div>
 		</div>
 	)
 }
-
+SearchField.propTypes = {
+	handleClick:PropTypes.func.isRequired,
+}
 export default SearchField
