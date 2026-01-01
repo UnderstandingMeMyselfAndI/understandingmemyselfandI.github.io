dbf410281fb1f6c5a15ce717c81dcb76f1a7a163
Brendan Oliver <brendan.oliver@flpdigital.com>
2026-01-01 00:27:47 +0000

    manifest screenshots for install prompt

--- src/components/visits/Vcn.jsx
+++ src/components/visits/Vcn.jsx
@@ -3,12 +3,10 @@ import useAppStore from '@/store/useAppStore'
 
 const Vcn = () => {
 	// increment visit count
-	
 
 	// // increment visit count
 	const incVC = useAppStore((s) => s.incVC)
-	incVC();
-
+	incVC()
 
 	const date = new Date()
 	const secondsSinceEpoch = Math.floor(date.getTime() / 1000)
@@ -16,11 +14,11 @@ const Vcn = () => {
 	// // set last visit date
 	const setLVD = useAppStore((s) => s.setLVD)
 	setLVD(secondsSinceEpoch)
-
+	// set first visit date
 	const setFVD = useAppStore((s) => s.setFVD)
 	setFVD(secondsSinceEpoch)
-	
-	return (<> </>)
+
+	return <> </>
 }
 
 export default Vcn
