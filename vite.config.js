a76b9c3c992be07535137049f6a9569aaff02aed
Brendan Oliver <brendan.oliver@flpdigital.com>
2026-02-15 16:09:58 +0000

    changed the output dir

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   build: {
     minify: 'esbuild',
     cssMinify: 'lightningcss',
-    outDir: './docs',
+    outDir: './dist',
     emptyOutDir: true,
     commonjsOptions: { transformMixedEsModules: true },
     cssCodeSplit: true,
