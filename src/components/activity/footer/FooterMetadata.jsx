75fa45a80a8aa3f3fe5bf47d0a02e5690469ea96
Brendan Oliver <brendan.oliver@flpdigital.com>
2026-05-02 01:26:49 +0100

    added version to footer

--- src/components/activity/footer/FooterMetadata.jsx
+++ src/components/activity/footer/FooterMetadata.jsx
@@ -18,14 +18,16 @@ function FooterMetadata() {
   // 	fetchAndCompareVersions();
   // }, []);
 
+  const buildMajor = metadata ? (metadata.buildMajor ? metadata.buildMajor : '') : ''
+  const buildMinor = metadata ? (metadata.buildMinor ? metadata.buildMinor : '') : ''
+  const buildRevision = metadata ? (metadata.buildRevision ? metadata.buildRevision : '') : ''
+
   const year = new Date().getFullYear()
 
   return (
     <div className='version-footer'>
       &copy; {year} <span className='ummi'>Ummi</span>
-      <div className='sf-footer-version'>
-        {`Version ${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildRevision}`}
-      </div>
+      <div className='sf-footer-version'>{`Version ${buildMajor}.${buildMinor}.${buildRevision}`}</div>
     </div>
   )
 }
